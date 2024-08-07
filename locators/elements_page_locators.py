fde57b5f58f82149595d8e6b4e923d477d860b54
Yaroslav Pimenov <slavk0@ukr.net>
2024-08-07 16:43:43 +0300

    added checkbox test

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class TextBoxPageLocators:
-
     FULL_NAME = (By.XPATH, '//input[@id="userName"]')
     EMAIL = (By.XPATH, '//input[@id="userEmail"]')
     CURRENT_ADDRESS = (By.XPATH, '//textarea[@id="currentAddress"]')
@@ -14,3 +13,11 @@ class TextBoxPageLocators:
     CREATED_CURRENT_ADDRESS = (By.XPATH, '//p[@id="currentAddress"]')
     CREATED_PERMANENT_ADDRESS = (By.XPATH, '//p[@id="permanentAddress"]')
 
+
+class CheckboxPageLocators:
+    EXPAND_ALL = (By.XPATH, '//button[@title="Expand all"]')
+    ITEM_LIST = (By.XPATH, '//span[@class="rct-title"]')
+    CHECKED_ITEMS = (By.CSS_SELECTOR, 'svg[class="rct-icon rct-icon-check"]')
+    # TITLE_ITEM = ".//ancestor::span[@class='rct-title']"
+    TITLE_ITEM = ".//ancestor::span[@class='rct-text']//span[@class='rct-title']"
+    OUTPUT_RESULT = (By.XPATH, '//span[@class="text-success"]')
