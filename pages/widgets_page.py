c6dc0d52a4eb7b904f13a8040fb38825f8690d17
Yaroslav Pimenov <slavk0@ukr.net>
2024-08-15 16:30:23 +0300

    changed func for accordion test

--- pages/widgets_page.py
+++ pages/widgets_page.py
@@ -7,20 +7,15 @@ class AccordianPage(BasePage):
 
     def check_accordian(self, num):
         if num == "first":
-            title_text = self.element_is_visible(self.locators.FIRST_SECTION).text
-            content = self.element_is_visible(self.locators.FIRST_CONTENT).text
-            # return [title_text, len(content)]
+            title_text = self.go_to_visible_element(self.locators.FIRST_SECTION).text
+            content = self.go_to_visible_element(self.locators.FIRST_CONTENT).text
         if num == "second":
-            title = self.element_is_visible(self.locators.SECOND_SECTION)
-            self.go_to_element(title)
-            title.click()
-            title_text = title.text
+            self.go_to_visible_element(self.locators.SECOND_SECTION).click()
+            title_text = self.go_to_visible_element(self.locators.SECOND_SECTION).text
             content = self.element_is_visible(self.locators.SECOND_CONTENT).text
         if num == "third":
-            title = self.element_is_visible(self.locators.THIRD_SECTION)
-            self.go_to_element(title)
-            title.click()
-            title_text = title.text
+            self.go_to_visible_element(self.locators.THIRD_SECTION).click()
+            title_text = self.go_to_visible_element(self.locators.THIRD_SECTION).text
             content = self.element_is_visible(self.locators.THIRD_CONTENT).text
         return [title_text, len(content)]
 
